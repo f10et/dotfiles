52bbf1e020e8d419082f15b0ae4a2c61503f0a95
Florian Zysset <admin@scboffspring.eu>
2013-01-02 02:38:49 +0100

    Adding .oh-my-zsh and mergin it

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -5,4 +5,4 @@ alias sg='script/generate'
 alias sd='script/destroy'
 
 alias migrate='rake db:migrate db:test:clone'
-alias create_and_clone= 'rake db:destoy db:create db:migrate'
+alias create_and_migrate= 'rake db:destoy db:create db:migrate'
