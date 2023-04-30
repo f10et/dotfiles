1af1848a11bbe4de027e3ac068aee5e607e4e9ab
f10et <florian@zysset.me>
2023-04-30 20:00:36 +0100

    some fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ sudo xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install --cask gpg-suite-no-mail
 
-mkdir ~/src/github.com/f10et
+mkdir -p ~/src/github.com/f10et
 cd ~/src/github.com/f10et 
 
 git clone https://github.com/f10et/dotfiles.git
@@ -12,6 +12,9 @@ cd ~
 ln -s ~/src/github.com/f10et/dotfiles ~/.dotfiles
 
 cd  ~/src/github.com/f10et/dotfiles
+
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/f10et/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
 script/bootstrap
 script/install
