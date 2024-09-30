184df5837443f85099a114ed661d18cdeabc88c8
Florian Zysset <florian@mindstone.com>
2024-09-30 17:53:45 +0100

    not sure

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,5 +1,9 @@
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+export LSCOLORS="gxfxcxdxBxegecabagacad" 
+
+alias ls='ls -lGH'
+
+# export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+
 export GREP_COLOR='1;33'
 export CLICOLOR=true
 
