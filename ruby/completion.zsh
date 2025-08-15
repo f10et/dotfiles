fbb18363ca1eb3baebc4291b84127841b1f2f86b
f10et <florian@zysset.me>
2025-08-15 21:54:43 +0100

    more fixes

--- ruby/completion.zsh
+++ ruby/completion.zsh
@@ -1,8 +1,8 @@
 # Stolen from
 #   https://github.com/sstephenson/rbenv/blob/master/completions/rbenv.zsh
 
-. "$HOME/.asdf/completions/asdf.bash"
-. "$HOME/.asdf/asdf.sh"
+#. "$HOME/.asdf/completions/asdf.bash"
+#. "$HOME/.asdf/asdf.sh"
 
 if [[ ! -o interactive ]]; then
     return
